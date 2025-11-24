1b274c2ff259cf28c97414df7b9dd4c6fa85922e
Abhay Kumar <abhaykumar1gkp@gmail.com>
2025-11-24 23:19:47 +0530

    Update ContentWrapper.jsx

--- src/components/contentWrapper/ContentWrapper.jsx
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -6,6 +6,8 @@ const ContentWrapper = ( { children } ) => {
     return <div className = "contentWrapper" > {children} </div>;
     };
 
+
 export default ContentWrapper;
 
 // this component use to center all the content of our project
+
