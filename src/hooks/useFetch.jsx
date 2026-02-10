684cc1523042f15cf3fa7826f9dfd7e3499d95e8
Abhay Kumar <abhaykumar1gkp@gmail.com>
2026-02-10 23:03:09 +0530

    Update useFetch.jsx

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -6,7 +6,6 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
-
     
     useEffect( () => {
         setLoading("loading...");
@@ -28,5 +27,6 @@ const useFetch = (url) => {
 };
 
 export default useFetch;
+
 
 
